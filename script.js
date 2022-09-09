0f1092b2034f6881c597b25af26f8a56a75392f1
reshma <reshmashaik0910@gmail.com>
2022-09-09 13:08:11 +0100

    changed functions

--- script.js
+++ script.js
@@ -1,132 +1,81 @@
-const display1El = document.querySelector('.display-1');
-const display2El = document.querySelector('.display-2');
-const numbersEl = document.querySelectorAll('.number');
-const operationEl = document.querySelectorAll('.operation');
-const equalEl = document.querySelector('.equal');
-const clearAllEl = document.querySelector('.all-clear');
-const clearLastEl = document.querySelector('.last-entity-clear');
-let dis1Num = '';
-let dis2Num = '';
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operation = document.querySelectorAll('.operation');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const clearLast = document.querySelector('.backSpace');
+let firstVal = '';
+let secondVal = '';
 let result = null;
 let operationType = '';
-let haveDot = false;
-
-numbersEl.forEach( number => {
+let containDot = false;
+numbers.forEach( number => {
   number.addEventListener('click', (e)=>{
-    if(e.target.innerText === '.' && !haveDot){
-      haveDot = true;
-    } else if(e.target.innerText === '.' && haveDot){
+    if(e.target.innerText === '.' && !containDot){
+      containDot = true;
+    } else if(e.target.innerText === '.' && containDot){
       return;
     }
-    dis2Num += e.target.innerText;
-    display2El.innerText = dis2Num;    
+    secondVal += e.target.innerText;
+    display.innerText = secondVal;    
   })
 })
-operationEl.forEach( operation => {
+operation.forEach( operation => {
   operation.addEventListener('click', (e)=> {
-    if (!dis2Num) return;
-    haveDot = false;
+    if (!secondVal) return;
+    containDot = false;
     const operationName = e.target.innerText;
-    if (dis1Num && dis2Num && operationName){
-      mathOperation();
-
-    }else{
-      result = parseFloat(dis2Num);
+    if (firstVal && secondVal && operationType){
+      display.innerText = '';
+      operate();
+      display.innerText = result;
     }
-    clearVar(operationName);
+    else{
+     result = parseFloat(secondVal);
+     display.innerText = result;
+    }
+    clearVar();
     operationType = operationName;
-    //console.log(result)
   })
 });
-function clearVar(name = ''){
-  dis1Num = dis2Num;
-  display2El.innerText = dis1Num;
-  dis2Num = '';
+function clearVar(){
+  firstVal = secondVal;
+  display.innerText = result;
+  secondVal = '';
 }
-
-function mathOperation() {
+function operate() {
   if (operationType === 'x') {
-    result = parseFloat(result) * parseFloat(dis2Num);
-    display2El.innerText = result;
+    result = parseFloat(result) * parseFloat(secondVal);
   } else if (operationType === '+') {
-    result = parseFloat(result) + parseFloat(dis2Num);
+    result = parseFloat(result) + parseFloat(secondVal);  
   } else if (operationType === '-') {
-    result = parseFloat(result) - parseFloat(dis2Num);
+    result = parseFloat(result) - parseFloat(secondVal); 
   } else if (operationType === '/') {
-    result = parseFloat(result) / parseFloat(dis2Num);
+    result = parseFloat(result) / parseFloat(secondVal);  
   }else if( operationType === '%'){
-    result = parseFloat(result) % parseFloat(dis2Num);
+    result = parseFloat(result) % parseFloat(secondVal);
+    display.innerText = result;
   }
 }
-// operation();
-equalEl.addEventListener('click', ()=> {
-  if (!dis1Num || !dis2Num) return;
-  haveDot = false;
-  mathOperation();
+// equal operation
+equal.addEventListener('click', ()=> {
+  if(!firstVal || !secondVal) return;
+  containDot = false;
+  operate();
   clearVar();
-  display2El.innerText = result;
-  tempResultEl.innerText = '';
-  dis2Num = result;
-  dis1Num = '';
+  display.innerText = result;
+  secondVal = result;
+  firstVal = '';
 })
-clearAllEl.addEventListener('click', ()=>{
-  dis1Num = '';
-  dis2Num = '';
-  display1El.innerText ='';
-  display2El.innerText ='';
-  result = '';
-  tempResultEl.innerText = '';
+//cleear operation
+clear.addEventListener('click', ()=>{
+ firstVal = '';
+ secondVal = '';
+ display.innerText ='';
+ result = '';
 });
-clearLastEl.addEventListener('click', () => {
-  display2El.innerText = '';
-  dis2Num = '';
-});
-window.addEventListener('keydown', (e)=>{
-  if(
-    e.key === '0' ||
-    e.key === '1' || 
-    e.key === '2' ||
-    e.key === '3' ||
-    e.key === '4' ||
-    e.key === '5' ||
-    e.key === '6' ||
-    e.key === '7' ||
-    e.key === '8' ||
-    e.key === '9' ||
-    e.key === '.' 
-  ){
-    clickButtonEl(e.key)
-    // console.log(e.key)
-  }else if(
-    e.key === '+' ||
-    e.key === '-' ||
-    e.key === '/' ||
-    e.key === '%' 
-  ){
-    clickOperation(e.key);
-  }
-  else if(e.key === '*'){
-    clickOperation('x')
-    // console.log(e.key)
-  } else if( e.key == "Enter" || e.key === '='){
-    clickEqual();
-  }
-  // console.log(e.key)
+//backspace operation
+clearLast.addEventListener('click', () => {
+  display.innerText = display.innerText.slice(0, -1);
+  secondVal = '';
 })
-function clickButtonEl(key) {
-  numbersEl.forEach(button => {
-    if (button.innerText === key) {
-      button.click();
-    }
-  })
-}
-function clickOperation(key){
-  operationEl.forEach( operation => {
-    if(operation.innerText === key){
-      operation.click()
-    }
-  })
-}
-function clickEqual(){
-  equalEl.click();
-}
